7d2af8b8e136fb746f4f6a262a288afa55fa8dda
Samp <samprdev@gmail.com>
2023-12-31 19:42:06 +0800

    added invading and rebelling

--- objects/empire.gd
+++ objects/empire.gd
@@ -7,13 +7,31 @@ class_name empire
 @export var label: String;
 @export_color_no_alpha var base_color: Color;
 @export var flag: Image;
+@export var original_territory: territory;
 
 @export var members: Array[territory];
 
 func get_neighbours():
-	var neighbours = [];
+	var neighbours = Array[territory];
 
 	for member in members:
 		for neighbour in member.neighbours:
-			if members.find(neighbour) == -1:
+			if members.find(neighbour) == -1 and neighbours.find(neighbour) == -1:
 				neighbours.append(neighbour);
+
+	return neighbours;
+
+func invade(enemy: empire):
+	for member in enemy.members:
+		member.setEmpire(self);
+		members.append(member);
+	enemy.members = [];
+
+func rebel():
+	var old_empire = original_territory.current_empire;
+	var members_list = old_empire.members;
+
+	members_list.remove_at(members_list.members.find(original_territory));
+
+	members = [original_territory];
+	original_territory.setEmpire(self);
--- objects/territory.gd
+++ objects/territory.gd
@@ -8,6 +8,12 @@ class_name territory
 
 @export_color_no_alpha var color: Color;
 @export var neighbours: Array[territory];
+@export var current_empire: empire;
 
-func setColor(base_color: Color):
-	color = color;
+func setEmpire(new_empire: empire):
+	current_empire = new_empire;
+	color = new_empire.base_color;
+	setColor();
+
+func setColor():
+	modulate = color;
